41b23d1ea106fd3907fb475b3855ec128a1f6457
Yu-Ming Chen <yminchen@umich.edu>
2020-08-30 16:49:11 -0400

    Add comments

--- hw2/sync_hw2.py
+++ hw2/sync_hw2.py
@@ -8,30 +8,30 @@ from pathlib import Path
 
 def sync_hw2():
   if 'google.colab' in str(get_ipython()):
+    # mount google drive to Colab
     from google.colab import drive
     drive.mount('/content/drive')
 
     dir = "drive/My Drive/MEAM517_colab/hw2/"
     git_url = "https://raw.githubusercontent.com/mposa/MEAM517/hw2/hw2/"
-
     files = ["quadrotor.py", "quadrotor_generator.py", "quad_sim.py", "trajectories.py"]
 
-    # Save files from Colab to Google drive
+    # 1. Copy files from Colab to Google drive
     if path.exists("quadrotor.py"):
       if not path.exists("drive/My Drive/MEAM517_colab/hw2"):
         # create hw folder in google drive
         Path(dir).mkdir(parents=True, exist_ok=True)
       [copyfile(file, dir + file) for file in files]
-    # Read files to Colab
+    # 2. Copy files into Colab
     else:
-      # If the hw folder doesn't exist in google drive, create the folder and create new homework scripts
-      if not path.exists("drive/My Drive/MEAM517_colab/hw2"):
+      # If the hw folder exists in google drive, copy the existing homework files from google drive to colab
+      if path.exists("drive/My Drive/MEAM517_colab/hw2"):
+        [copyfile(dir + file, file) for file in files]
+      # Otherwise, create the folder and create new homework scripts
+      else: 
         # create hw folder in google drive
         Path(dir).mkdir(parents=True, exist_ok=True)
         # download homework scirpts to colab 
         [urllib.request.urlretrieve(git_url + file, file) for file in files]
         # copy the new scripts to hw folder in google drive
         [copyfile(file, dir + file) for file in files]
-      # Otherwise, copy the existing homework scirpts from google drive to colab
-      else: 
-        [copyfile(dir + file, file) for file in files]
