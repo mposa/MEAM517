2c1c5bc194c2b61685a84682f31451497e46a236
Yu-Ming Chen <yminchen@umich.edu>
2020-09-13 11:27:34 -0400

    Add a shell script for colab

---
+++ colab_drake_setup.sh
@@ -0,0 +1,7 @@
+# This file is added to reduced the size of setup code in Colab jupyter notebook
+
+curl -s https://raw.githubusercontent.com/mposa/MEAM517/hw2/colab_drake_setup.py > drake_setup.py
+python3 drake_setup.py
+rm -rf sample_data
+rm -rf drake.tar.gz
+rm -rf drake_setup.py
