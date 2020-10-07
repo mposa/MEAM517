b339a6daa3de8857ad54d9b0a973eeb76175094a
William Yang <yangwill@seas.upenn.edu>
2020-10-07 15:17:32 -0400

    adding end effector z pos as a constraint

--- hw5/kinematic_constraints.py
+++ hw5/kinematic_constraints.py
@@ -8,10 +8,7 @@ def cos(theta):
 def sin(theta):
   return AutoDiffXd.sin(theta)
 
-
 def landing_constraint(vars):
-  # import AutoDiffXd.cos as cos
-  # import AutoDiffXd.sin as sin
   '''
   Impose a constraint such that if the ball is released at final state xf, 
   it will land a distance d from the base of the robot 
@@ -26,8 +23,11 @@ def landing_constraint(vars):
                   [-l*cos(q[0]) - l*cos(q[0] + q[1]) - l*cos(q[0] + q[1] + q[2])]])
   vel = np.array([[l*qdot[2]*cos(q[0] + q[1] + q[2]) + qdot[0]*(l*cos(q[0]) + l*cos(q[0] + q[1]) + l*cos(q[0] + q[1] + q[2])) + qdot[1]*(l*cos(q[0] + q[1]) + l*cos(q[0] + q[1] + q[2]))],
                   [l*qdot[2]*sin(q[0] + q[1] + q[2]) + qdot[0]*(l*sin(q[0]) + l*sin(q[0] + q[1]) + l*sin(q[0] + q[1] + q[2])) + qdot[1]*(l*sin(q[0] + q[1]) + l*sin(q[0] + q[1] + q[2]))]])
-  
+
   # TODO: Express the landing constraint as a function of q, qdot, and t_land
+  # constraint_eval[0]: Eq (15)
+  # constraint_eval[1]: Eq (16)
+  # constraint_eval[2]: Eq (18)
 
   return constraint_eval
 
@@ -35,7 +35,6 @@ def AddFinalLandingPositionConstraint(prog, xf, d, t_land):
 
   # TODO: Add the landing distance equality constraint as a system of inequality constraints 
   # using prog.AddConstraint(landing_constraint, lb, ub, vars) 
-
+  
   # TODO: Add a constraint that t_land is positive
 
-  pass
